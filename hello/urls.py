d1c35fc2d4ae74815b5db4d0d98819e446d2f918
1Rodrigues <klebesonr2018@gmail.com>
2026-09-16 08:45:10 -0300

    final

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello-world'),
+    path('', views.hello_world, name='index'),
+    path('detalhes', views.detalhes, name ='detalhes'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'hello/index.html')
+
+def detalhes(request):
+    return render(request, 'hello/detalhes.html')
